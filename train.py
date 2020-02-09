3c50130426751f820d43fed7f6ad0ed1c3a98807
Ethan Cumberland <ethancumberland@mail.com>
2020-02-09 17:31:08 +0000

    Copies full tweet text from the Trump Tweets archive to the harvard dataset

--- train.py
+++ train.py
@@ -2,8 +2,18 @@ import pandas as pd
 import ujson as json
 
 tt_df = pd.read_json("Datasets/trumptweets.json")
-records = map(json.loads, open('Datasets/realdonaldtrump.ndjson', encoding="utf8"))
-rdt_df = pd.DataFrame.from_records(records)
+rdt_df = pd.DataFrame.from_records(map(json.loads, open('Datasets/realdonaldtrump.ndjson', encoding="utf8")))
 
-print (tt_df)
-print (rdt_df)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_columns', None)
+
+print(tt_df)
+print(rdt_df)
+
+for row_index, row in rdt_df.iterrows():
+    tt_row = tt_df.loc[tt_df['id_str'] == row['id']]
+    if not tt_row.empty:
+        rdt_df.at[row_index, 'text'] = tt_row['text'].item()
+
+pd.reset_option('display.max_colwidth')
+pd.reset_option('display.max_columns')
