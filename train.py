d46b0eefbf27f3be52555652f4d3d6a4776a406b
ed_gray <edwardgray2903@gmail.com>
2020-03-17 18:34:01 +0000

    Attempt some bag of words on tweet text

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_columns', None)
@@ -21,5 +22,27 @@ dataset.reset_index(drop=True, inplace=True)
 # shuffle the dataset
 dataset = dataset.sample(frac=1)
 
+#https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+
+#preprocess, tokenize and filter stopwords and produce bag of words from tweet text
+#produces sparse matrix, where each row represents a tweet and the given tweets word occurences
+count_vect = CountVectorizer()
+counts = count_vect.fit_transform(dataset['text'])
+
+#divides occurences by number of words in tweet
+tfidf_transformer = TfidfTransformer(use_idf=False)
+tf = tfidf_transformer.fit_transform(counts)
+
+#add column, using row 0 to initialize datatype
+dataset['Term Frequencies'] = tf[0]
+
+#add data to column
+for i, row in dataset.iterrows():
+    dataset.at[i, 'Term Frequencies'] = tf[i]
+
+print(dataset.head(5))
+
 pd.reset_option('display.max_colwidth')
 pd.reset_option('display.max_columns')
+
+
