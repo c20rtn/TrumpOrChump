a01a667d92c212af6d8254479a53ef5ed61e9635
Ethan Cumberland <ethancumberland@mail.com>
2020-03-19 20:53:11 +0000

    Refactored code into separate files

---
+++ models.py
@@ -0,0 +1,30 @@
+# LOGISTIC REGRESSION
+def logistic_regression(X_train, y_train):
+    from sklearn.linear_model import LogisticRegression
+
+    lr_model = LogisticRegression(random_state=0, max_iter=5000)
+    return lr_model.fit(X_train, y_train)
+
+
+# NAIVE BAYES
+def naive_bayes(X_train, y_train):
+    from sklearn.naive_bayes import GaussianNB
+
+    nb_model = GaussianNB()
+    return nb_model.fit(X_train, y_train)
+
+
+# SVM
+def svm(X_train, y_train):
+    from sklearn.svm import LinearSVC
+
+    svc_model = LinearSVC(random_state=0, tol=1e-03, max_iter=5000)
+    return svc_model.fit(X_train, y_train)
+
+
+# MLP
+def mlp(X_train, y_train):
+    from sklearn.neural_network import MLPClassifier
+
+    mlp_model = MLPClassifier(random_state=0)
+    return mlp_model.fit(X_train, y_train)
--- train.py
+++ train.py
@@ -2,20 +2,13 @@ import pandas as pd
 import feature_extraction
 from sklearn.model_selection import train_test_split
 
-
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_columns', None)
 
+# LOAD DATA
 # read in individual datasets
 trump_tweets = pd.read_json("Datasets/trump_tweets.json")
 general_tweets = pd.read_json("Datasets/general_tweets.json")
-
-# print(trump_tweets['favorite_count'].mean())
-# print(trump_tweets['retweet_count'].mean())
-# print(general_tweets['favorite_count'].mean())
-# print(general_tweets['retweet_count'].mean())
-# print(feature_extraction.extract_features(general_tweets)['no_media'].mean())
-# print(feature_extraction.extract_features(trump_tweets)['no_media'].mean())
 
 # label the datasets
 trump_tweets['label'] = 1
@@ -33,83 +26,64 @@ X_test.reset_index(inplace=True, drop=True)
 y_train.reset_index(inplace=True, drop=True)
 y_test.reset_index(inplace=True, drop=True)
 
-# X_train = feature_extraction.extract_features(X_train)
-# X_test = feature_extraction.extract_features(X_test)
-
-# DATA SCALING
-from sklearn.preprocessing import StandardScaler
-
-# scaler = StandardScaler()
-# scaler.fit(X_train)
-# X_train = scaler.transform(X_train)
-# X_test = scaler.transform(X_test)
 
 # TEXT EXTRACTION
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 # https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
 
 # preprocess, tokenize and filter stopwords and produce bag of words from tweet text
-# produces sparse matrix, where each row represents a tweet and the given tweets word occurences
+# produces sparse matrix, where each row represents a tweet and the given tweets word occurrences
 count_vect = CountVectorizer()
 counts = count_vect.fit_transform(X_train['text'])
 
-# divides occurences by number of words in tweet
+# divides occurrences by number of words in tweet
 tfidf_transformer = TfidfTransformer(use_idf=False)
-tf = tfidf_transformer.fit_transform(counts)
-# print(X_train.to_numpy())
 
-from scipy import sparse
-# print(sparse.csr_matrix(feature_extraction.extract_features(X_train).values))
-joined_train = sparse.hstack([tf, sparse.csr_matrix(feature_extraction.extract_features(X_train).values)])
-
+X_train_tf = tfidf_transformer.fit_transform(counts)
 X_test_tf = tfidf_transformer.transform(count_vect.transform(X_test['text']))
 
-joined_test = sparse.hstack([X_test_tf, sparse.csr_matrix(feature_extraction.extract_features(X_test).values)])
 
-# LOGISTIC REGRESSION
-from sklearn.linear_model import LogisticRegression
-
-# lr = LogisticRegression(random_state=0, max_iter=5000)
-# lr.fit(joined_train, y_train)
-# y_pred = lr.predict(joined_test)
+# EXTRACT FEATURES
+X_train = feature_extraction.extract_features(X_train)
+X_test = feature_extraction.extract_features(X_test)
 
 
-# NAIVE BAYES
-from sklearn.naive_bayes import GaussianNB
+# DATA SCALING
+from sklearn.preprocessing import StandardScaler
 
-# nb = GaussianNB()
-# nb.fit(X_train, y_train)
-# y_pred = nb.predict(X_test)
-
-
-# SVM
-from sklearn.svm import LinearSVC
-
-svc = LinearSVC(random_state=0, tol=1e-03, max_iter=10000)
-svc.fit(joined_train, y_train)
-y_pred = svc.predict(joined_test)
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 
-# MLP
-from sklearn.neural_network import MLPClassifier
+# JOIN SCALED FEATURES AND TEXT MEASURES
+from scipy import sparse
 
-# mlp = MLPClassifier(random_state=0)
-# mlp.fit(X_train, y_train)
-# y_pred = mlp.predict(X_test)
+joined_train = sparse.hstack([X_train_tf, sparse.csr_matrix(X_train)])
+joined_test = sparse.hstack([X_test_tf, sparse.csr_matrix(X_test)])
+
+
+# RUN TRAINING
+import models
+
+model = models.logistic_regression(joined_train, y_train)
+# model = models.naive_bayes(X_train, y_train)
+# model = models.svm(joined_train, y_train)
+# model = models.mlp(joined_train, y_train)
+
+y_pred = model.predict(joined_test)
+
 
 # ACCURACY MEASURES
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score
 
-print(accuracy_score(y_test, y_pred))
-
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 
 
 pd.reset_option('display.max_colwidth')
 pd.reset_option('display.max_columns')
-
-
